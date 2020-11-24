c0098d1323f14da165be430b017cd257d80d059e
Alexandre Maia <alex.maia.vc@gmail.com>
2020-11-24 07:35:32 +0000

    Config Controller structure

---
+++ controllers/user.controller.js
@@ -0,0 +1,5 @@
+const models = require('../models');
+
+exports.users = function(req, res, next) {
+    res.send('We are in users controller');
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,16 @@
 const express = require('express');
 let router = express.Router();
 
+// Set Controller
+const UserController = require('../controllers/user.controller.js')
+
 
 router.get('/', (req, res) => {
     res.send('We are home');
 });
+
+// Set User Routes
+router.get('/users', UserController.users);
+
 
 module.exports = router;
